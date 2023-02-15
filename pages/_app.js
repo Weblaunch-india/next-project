9548ebdfabd649d94c287858b4da1c9f5eec01d9
Mohd-Muneeb <mohdmuneeb1307@gmail.com>
2023-02-15 22:17:10 +0530

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+// import "../styles/globals.css";
+import "../bootstrap/css/index.css";
+import "../bootstrap/css/add.css";
+import "../bootstrap/fontawesome/css/all.css";
+import "../bootstrap/boxicons/css/boxicons.min.css";
+import "../bootstrap/css/style.css";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
+
+const Document = () => {
+	return (
+        <Html>
+            <Head>
+				<link
+					rel="icon"
+					type="image"
+					href="../bootstrap/images/PNG.png"
+					sizes="192x192"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+};
+
+export default Document;
