03689820f62d0ea86c3f6b0585ceaa1f3acb0e5e
Mohd-Muneeb <mohdmuneeb1307@gmail.com>
2023-03-09 21:37:32 +0530

    Updating carousal images

--- components/Carousal.jsx
+++ components/Carousal.jsx
@@ -8,9 +8,14 @@ import css from "../bootstrap/css/carousal.module.css";
 
 function Carousal() {
 	const links = [
-		"https://raw.githubusercontent.com/Weblaunch-india/next-project/main/bootstrap/images/home_images/1-1.jpeg",
-		"https://raw.githubusercontent.com/Weblaunch-india/next-project/main/bootstrap/images/home_images/1-2.jpeg",
-		"https://raw.githubusercontent.com/Weblaunch-india/next-project/main/bootstrap/images/home_images/1-3.jpg",
+		"https://firebasestorage.googleapis.com/v0/b/spoorthy-6b2ed.appspot.com/o/carousal%2FWhatsApp%20Image%202023-03-09%20at%2021.11.46%20(1).jpeg?alt=media&token=a047a331-3416-4a87-a899-8c93a1971da7",
+		"https://firebasestorage.googleapis.com/v0/b/spoorthy-6b2ed.appspot.com/o/carousal%2FWhatsApp%20Image%202023-03-09%20at%2021.11.46%20(2).jpeg?alt=media&token=38a98d32-1d89-4e14-aa3a-002842680cef",
+		"https://firebasestorage.googleapis.com/v0/b/spoorthy-6b2ed.appspot.com/o/carousal%2FWhatsApp%20Image%202023-03-09%20at%2021.11.46.jpeg?alt=media&token=ce6c635c-93ba-43e7-b7d7-326f85892d33",
+		"https://firebasestorage.googleapis.com/v0/b/spoorthy-6b2ed.appspot.com/o/carousal%2FWhatsApp%20Image%202023-03-09%20at%2021.11.47%20(1).jpeg?alt=media&token=69c8bcdd-7945-44c1-82cc-06b6d19a2449",
+		"https://firebasestorage.googleapis.com/v0/b/spoorthy-6b2ed.appspot.com/o/carousal%2FWhatsApp%20Image%202023-03-09%20at%2021.11.47%20(2).jpeg?alt=media&token=24cf68ff-3b04-45e6-84df-6dd12f4ccf8e",
+		"https://firebasestorage.googleapis.com/v0/b/spoorthy-6b2ed.appspot.com/o/carousal%2FWhatsApp%20Image%202023-03-09%20at%2021.11.47%20(3).jpeg?alt=media&token=5713fab3-ac28-4952-8d19-4b1f45b6effb",
+		"https://firebasestorage.googleapis.com/v0/b/spoorthy-6b2ed.appspot.com/o/carousal%2FWhatsApp%20Image%202023-03-09%20at%2021.11.48%20(1).jpeg?alt=media&token=c9354a8d-5853-4955-90df-5926fd92dd94",
+		"https://firebasestorage.googleapis.com/v0/b/spoorthy-6b2ed.appspot.com/o/carousal%2FWhatsApp%20Image%202023-03-09%20at%2021.11.48.jpeg?alt=media&token=733ab2ee-db73-4398-b9f8-93ffe4ccb1d8",
 	];
 	const [Active, setActive] = useState(0);
 
@@ -18,14 +23,10 @@ function Carousal() {
 		<div className={css.carousalContainer}>
 			<button
 				onClick={(e) => {
-					if (Active == 0) {
-						setActive(2);
-					} else if (Active == 1) {
-						setActive(0);
-					} else {
-						setActive(1);
-					}
-					console.log(Active);
+					const newActive = Active - 1;
+					setActive(
+						newActive < 0 ? 7 : newActive
+					);
 				}}
 				className={css.left}
 			>
@@ -43,22 +44,37 @@ function Carousal() {
 							}
 							key={elem}
 						>
-							<img className={elem == links[0] ? css.image1 : css.image} src={elem} />
-							{elem === links[0] ? <p className={css.overlayText}>Spoorthy Organisation</p> : <></>}
+							<img
+								className={
+									elem ==
+									links[0]
+										? css.image1
+										: css.image
+								}
+								src={elem}
+							/>
+							{elem === links[0] ? (
+								<p
+									className={
+										css.overlayText
+									}
+								>
+									Spoorthy
+									Organisation
+								</p>
+							) : (
+								<></>
+							)}
 						</div>
 					);
 				})}
 			</div>
 			<button
 				onClick={(e) => {
-					if (Active == 0) {
-						setActive(1);
-					} else if (Active == 1) {
-						setActive(2);
-					} else {
-						setActive(0);
-					}
-					console.log(Active);
+					const newActive = Active + 1;
+					setActive(
+						newActive >= 7 ? 0 : newActive
+					);
 				}}
 				className={css.right}
 			>
